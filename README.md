082d2bf23edabce7b9d89a6a8013857291cfc8d2
Aryan <70486657+aryanation12@users.noreply.github.com>
2021-07-14 12:13:58 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Conference Call
+A conference call implementation using WebRTC, Socket.io and Node.js.
+
+
+# Getting Started
+- Run `npm install`
+- CD src
+- node app.js
+
+
+# Features
+- Multi-participants
+- Toggling of video stream
+- Toggling of audio stream (mute & unmute)
+- Screen sharing
+- Text chat
+- Mute individual participant
+- Expand participants' stream
+- Screen Recording
+- Video Recording
+
+
+
+
+
+
+
---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const hostname = "localhost";
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+const stream = require("./ws/stream");
+const io = require("socket.io")(server);
+const path = require("path");
+const favicon = require("serve-favicon");
+
+app.use(favicon(path.join(__dirname, "favicon.ico")));
+app.use("/assets", express.static(path.join(__dirname, "assets")));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+server.listen(port, hostname, function (err) {
+  if (err) {
+    throw err;
+  }
+  console.log("server listening on: ", hostname, ":", port);
+});
+io.of("/stream").on("connection", stream);
+//console.log("Express server listening on port %d", server.address().port);
+//server.listen(3031);
